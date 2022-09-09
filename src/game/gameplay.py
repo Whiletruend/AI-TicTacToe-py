f81042c3f9e40c4f7fe87f7cb4094b97cf08caf2
SkaaYz <bongage.benjamin@gmail.com>
2022-09-09 17:54:17 +0300

    (09/09/2022 17:53) + Added "protect" system. When a button is clicked, we can't change the mark sign.
    ~ Moved part of the code in gameplay.py.

--- src/game/gameplay.py
+++ src/game/gameplay.py
@@ -4,16 +4,21 @@
 class Gameplay:
     # Construct (init)
     def __init__(self):
-        self.current_mark = ""
+        self.current_mark = "x"
+        self.buttons_clicked = []
         self.actual_try = 0
 
-    # Show func
+    # Button Click func
     def Button_Click(self, button_id, buttons_array):
-        self.actual_try = self.actual_try + 1
-
-        if self.actual_try % 2 == 0:
-            self.current_mark = "x"
+        if buttons_array[button_id - 1] in self.buttons_clicked:
+            pass
         else:
-            self.current_mark = "o"
+            self.actual_try = self.actual_try + 1
 
-        buttons_array[button_id - 1].configure(text=self.current_mark)
+            buttons_array[button_id - 1].configure(text=self.current_mark)
+            self.buttons_clicked.append(buttons_array[button_id - 1])
+
+            if self.actual_try % 2 == 0:
+                self.current_mark = "x"
+            else:
+                self.current_mark = "o"
